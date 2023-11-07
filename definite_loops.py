2e71b704272ae506a197fc7bc03a10d740492bf6
Nathan Hopkins <nhopkins0991@hotmail.com>
2023-11-06 20:03:14 -0500

    mod 3 update

--- definite_loops.py
+++ definite_loops.py
@@ -1,36 +1,37 @@
-def print_pyramid(height):
-    if height > 0:
-        for i in range(1, height + 1):
-            # Print spaces
-            for j in range(height - i):
-                print(" ", end="")
-            # Print increasing numbers
-            for j in range(1, i + 1):
-                print(j, end="")
-            # Print decreasing numbers
-            for j in range(i - 1, 0, -1):
-                print(j, end="")
-            print()
-    elif height < 0:
+#Nathan Hopkins
+#Date November 6th, 2023
+#Module 3  First Lab Pyramid
+def get_input() -> int:
+    while True:
+        try:
+            height = int(input("Enter the height of the pyramid: "))
+            return height
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+def write_pyramid_row(row: int, height: int) -> None:
+    # Print spaces before numbers
+    for i in range(height - row):
+        print(" ", end="")
+    # Print increasing numbers
+    for i in range(1, row + 1):
+        print(i, end="")
+    # Print decreasing numbers (if there are any)
+    for i in range(row - 1, 0, -1):
+        print(i, end="")
+    print()
+
+def create_pyramid(height: int) -> None:
+    # Invert the pyramid if height is negative
+    if height < 0:
         height = abs(height)
         for i in range(height, 0, -1):
-            # Print spaces
-            for j in range(height - i):
-                print(" ", end="")
-            # Print increasing numbers
-            for j in range(1, i + 1):
-                print(j, end="")
-            # Print decreasing numbers
-            for j in range(i - 1, 0, -1):
-                print(j, end="")
-            print()
+            write_pyramid_row(i, height)
     else:
-        print("Invalid input. Height must be a non-zero integer.")
+        # Loop through write_pyramid_row to create the full pyramid
+        for i in range(1, height + 1):
+            write_pyramid_row(i, height)
 
-# Main function
 if __name__ == "__main__":
-    try:
-        height = int(input("Enter the height of the pyramid: "))
-        print_pyramid(height)
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
+    height = get_input()
+    create_pyramid(height)
